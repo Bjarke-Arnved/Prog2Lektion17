c3181d6ae9be9d2570ecfa3ff067a7b0ac4a0d41
Bjarke-Arnved <eaa23bjar@students.eaaa.dk>
2024-04-09 19:22:44 +0200

    finished exercise03

--- src/opgave03/Opgave03.java
+++ src/opgave03/Opgave03.java
@@ -1,4 +1,20 @@
 package opgave03;
 
+import opgave03.models.DropOutStack;
+
 public class Opgave03 {
+    public static void main(String[] args) {
+        DropOutStack<String> heros = new DropOutStack<String>(3);
+        heros.push("Poul Benden");
+        heros.push("Emily Boll");
+        heros.push("Kenjo Fusaiyuki");
+        heros.push("Salla Telgar");
+        System.out.println(heros.size());
+
+        heros.pop();
+        heros.clear();
+        System.out.println(heros.size());
+
+
+    }
 }
--- src/opgave03/models/DropOutStack.java
+++ src/opgave03/models/DropOutStack.java
@@ -5,44 +5,106 @@ import opgave01.models.StackEaaa;
 public class DropOutStack<E> implements StackEaaa<E> {
 
     private int maxSize;
-
+    private Node<E> top;
+    private Node<E> bottom;
     public DropOutStack(int maxSize) {
         this.maxSize = maxSize;
+        top = new Node<E>();
     }
 
     @Override
     public void push(E element) {
+        Node<E> node = new Node<E>(element);
 
+        if(isEmpty()) {
+            top = node;
+            bottom = top;
+        }
+        else {
+            node.setNext(top);
+            top.setPrevious(node);
+            top = node;
+            if(size() < maxSize) {
+                bottom = bottom.getPrevious();
+                bottom.setNext(null);
+            }
+        }
     }
 
     @Override
     public E pop() {
-        return null;
+        Node<E> nodeToBePoped = top;
+        top = top.getNext();
+        top.setPrevious(null);
+        return nodeToBePoped.getElement();
     }
 
     @Override
     public E peek() {
-        return null;
+        return top.getElement();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        boolean status = false;
+        if(top == null) {
+            status = true;
+        }
+        return status;
     }
 
     @Override
     public void clear() {
-
+        top = null;
+        bottom = null;
     }
 
     @Override
     public int size() {
-        return 0;
+        int counter = 0;
+        Node<E> node = top;
+        if(top != null) {
+            do {
+                counter++;
+                node = node.getNext();
+            } while(node.getNext() != null);
+        }
+        else {
+            counter = 0;
+        }
+        return counter;
     }
 
     private class Node<T> {
+        private Node() {}
+        private Node(T element) {
+            this.element = element;
+        }
         T element;
         Node<T> next;
         Node<T> previous;
+
+        private T getElement() {
+            return element;
+        }
+        private void setElement(T element) {
+            this.element = element;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public Node<T> getPrevious() {
+            return previous;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public void setPrevious(Node<T> previous) {
+            this.previous = previous;
+        }
     }
 }
